18c9030a4e617d4829cd93a1d2594762f08c4143
Lacrif <christophe.lacombe@hotmail.fr>
2026-05-26 18:24:31 +0200

    chore: add coverage provider v8 and all:true to include untested files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
+      provider: "v8",
       include: ["src/**/*.ts"],
+      all: true,
     },
   },
 });
